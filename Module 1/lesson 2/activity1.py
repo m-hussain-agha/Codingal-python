a84fa7ff94f7f9ecfe00a577bb0f648cfc4dd6b4
m-hussain-agha <m.hussainagha.6a.21152@gmail.com>
2025-06-25 04:31:40 -0700

    submit

---
+++ Module 1/lesson 2/activity1.py
@@ -0,0 +1,19 @@
+name ="Penguin"
+age =15
+is_student = True
+weight = 38.5
+
+print("Name :", name)
+print("Data Type of Name is", type (name))
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print("Data type of is_student",type(is_student))
+print("weight:", weight)
+print("Data Type of Weight is",type(weight))
+print("\n After Type CAsting....")
+age =str (age)
+print(age)
+print("Data type of Age is", type(age))
+weight = int(weight)
+print(weight)
+print("Dta Type of Weight is", type(weight))
---
+++ Module 1/lesson 2/activity2.py
@@ -0,0 +1,19 @@
+num1 = 45
+num2 = 3
+
+print("Number 1", num1)
+print("Number 2",num2)
+print("Addition:", num1+num2)
+print("Difference :", num1-num2)
+print("product :", num1*num2)
+print("division :", num1/num2)
+print("floor divisoin :", num1//num2)
+print("Modules Operation :",num1%num2)
+print("square :", num1**num2)
+print("Square Root :", num1**0.5)
+print("Equal ?", num1==num2)
+print("Number 1 greater?", num1>num2)
+print("Number 2 greater?", num1<num2)
+print("Not equal ?", num1!=num2)
+result = num1/2+num2**2+10
+print("result of given equation is:",result)
