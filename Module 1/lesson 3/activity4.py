754eafef6af3acf013a361fed8bc0e27c4c43a98
m-hussain-agha <m.hussainagha.6a.21152@gmail.com>
2025-07-01 04:24:51 -0700

    submit

---
+++ Module 1/lesson 3/activity4.py
@@ -0,0 +1,10 @@
+import datetime# using now() to get current time
+current_time = datetime.datetime.now()
+
+# Printing value of now.
+print ("Time now at greenwich meridian is : ", end = "")
+print ( current_time)
+
+# print calender of year 2021
+import calendar
+print("\n", calendar.calendar(2021))
