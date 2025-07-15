2a775b4e2089864e2d93c5c7467f4d583085ab17
m-hussain-agha <m.hussainagha.6a.21152@gmail.com>
2025-07-15 04:41:44 -0700

    submit

---
+++ project/data structure.py
@@ -0,0 +1,30 @@
+import random
+
+def guess_the_number():
+    # Generate a random number between 1 and 100
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+    guessed = False
+
+    print("Welcome to the Number Guessing Game!")
+    print("I have selected a number between 1 and 100. Can you guess it?")
+
+    while not guessed:
+        try:
+            # Get user input
+            user_guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            # Check the guess
+            if user_guess < number_to_guess:
+                print("Too low! Try again.")
+            elif user_guess > number_to_guess:
+                print("Too high! Try again.")
+            else:
+                guessed = True
+                print(f"Congratulations! You've guessed the number {number_to_guess} in {attempts} attempts.")
+        except ValueError:
+            print("Please enter a valid integer.")
+
+# Run the game
+guess_the_number()
