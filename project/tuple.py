d0e038a5bf68657cc43c90e8119cc37ff31fc1e9
m-hussain-agha <m.hussainagha.6a.21152@gmail.com>
2025-07-22 04:46:08 -0700

    submit

---
+++ project/tuple.py
@@ -0,0 +1,10 @@
+# Function to convert tuple to list
+def tuple_to_list(tup):
+    return list(tup)
+
+# Example usage
+my_tuple = (1, 2, 3, 4, 5)
+my_list = tuple_to_list(my_tuple)
+
+print("Tuple:", my_tuple)
+print("List:", my_list)
